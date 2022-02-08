793c28023adc919a567f4d9aea6ed2ffadff42c6
mrIsaacDolphin <isaac_dolphin@outlook.com>
2022-02-08 16:34:14 +0000

    Upgrade to main program, changed buffer of array.

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ A random password generator for my SDS coursework at the University of Warwick.
 - Generates a password from a given array of characters.
 - Basic error checking, able to determine what's been entered and check whether it's acceptable.
 - Allows a user to pick between 8 and 24 characters, this is the recommended requirement for a password.
-- Prints the password that you've generated to a text file, if you choose.
+- Prints the password that you've generated to a text file.
 
 # To-do
 - Encrypt the password inside of the text file in a cypher of some description (currently unknown).
-- Manage the memory effectively, reset the memory allocation to 0 (memset).
+- Look into "memset" for memory management efficiency.
 
 # Issues
-- String printed to the text file is currently a weird box? Issue with char printing from array.
+- String being printed is currently printing a "zero byte".
--- randomPasswordGenerator.c
+++ randomPasswordGenerator.c
@@ -6,13 +6,14 @@
 // Create variable for the answer to the request of the variable.
 int requestLength;
 int shouldStore;
+char passwordStr;
 
 int main() {
     // Creates a text file called passwordOutput.txt with write permissions (hence "w" as mode), and opens the channel for writing data.
     FILE *exportData = fopen("passwordOutput.txt", "w");
     
     // Creating an array of characters and requesting the size of the array from the program.
-    char charArr[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$^&*()[]?";
+    char charArr[74] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$^&*()[]?";
     int arrLen = sizeof charArr;
 
     printf("Please specify the length of the password you'd like to generate, between 8 and 24.\n");
@@ -23,13 +24,13 @@ int main() {
     // If the character entered into the system is not a valid number, or is greater than 24 or less than 8, print this statement and exit the program.
     if (requestLength > 24 || requestLength < 8) {
         system("clear");
-        printf("\nError: This character is not supported by the system. Please enter a value between 8 and 24.\n");
+        printf("Error: This character is not supported by the system. Please enter a value between 8 and 24.\n");
         sleep(2);
+        system("clear");
         main();
     }
     // If the number entered into the system is less than 24, run this code and exit the program.
     else if (requestLength <= 24 || requestLength >= 8) {
-        const char passwordStr;
         // Initiating variable to pull random items out of an array.
         time_t pullRandom;
         srand((unsigned)time(&pullRandom));
@@ -38,22 +39,11 @@ int main() {
         // For each number in requestLength, run the code inside the curly braces.
         for (int x = 0; x < requestLength; x++) {
             // Printing a random item from the array to the amount of requested items.
-            char passwordStr = printf("%c", charArr[rand()%arrLen]);
-        }
-        printf("\nWould you like to store your shiny, new password in a .txt file? 1 for yes, 2 for no.\n");
-        scanf("%d", &shouldStore);
-        if (shouldStore == 1) {
-            // Writes the data inside of passwordStr to the exportData variable, pointing to a text file. 
+            printf("%c", charArr[rand()%arrLen]);
             fprintf(exportData, "%c", passwordStr);
-            return 0;
         }
-        else if (shouldStore == 2) {
-            system("clear");
-            printf("Suit yourself! Thank you for using my random password generator.\n");
-            sleep(2);
-            system("clear");
-            exit(0);
-        }
+        printf("\n");
+        return 0;
     }
     // Close the write stream for appending data to the file.
     fclose(exportData);
